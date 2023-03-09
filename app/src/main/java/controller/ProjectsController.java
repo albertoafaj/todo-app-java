103f8dd3582b0234891b1bca93a3cd64f187317c
Alberto Araújo <albertoafaj@hotmail.com>
2023-03-09 17:43:13 -0300

    feat: add method get projects

--- app/src/main/java/controller/ProjectsController.java
+++ app/src/main/java/controller/ProjectsController.java
@@ -3,6 +3,10 @@ package controller;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import model.Projects;
 import util.ConnectionFactory;
 
@@ -28,5 +32,35 @@ public class ProjectsController {
     } finally {
       ConnectionFactory.closeConnection(conn, statement);
     }
+  }
+
+  public List<Projects> getAll(int id) {
+    String sql = "SELECT * FROM projects";
+    Connection conn = null;
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+    List<Projects> projects = new ArrayList<Projects>();
+    try {
+      conn = ConnectionFactory.getConnection();
+      statement = conn.prepareStatement(sql);
+      resultSet = statement.executeQuery();
+      while (resultSet.next()) {
+        Projects project = new Projects();
+        project.setId(resultSet.getInt("id"));
+        project.setName(resultSet.getString("name"));
+        project.setDescription(resultSet.getString("description"));
+        project.setDateCreated(resultSet.getDate("dateCreated"));
+        project.setDateLastUpdate(resultSet.getDate("dateLastUpdate"));
+        project.setIdOwnerUser(resultSet.getInt("IdOwnerUser"));
+        projects.add(project);
+      }
+
+    } catch (Exception error) {
+      throw new RuntimeException("Erro ao buscar o projeto", error);
+    } finally {
+      ConnectionFactory.closeConnection(conn, statement, resultSet);
+    }
+
+    return projects;
   }
 }
