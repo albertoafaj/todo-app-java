ea867fe65445c6914c039c7c7194284b4f4f8ef3
Alberto Araújo <albertoafaj@hotmail.com>
2023-03-10 21:09:43 -0300

    fix: change deprecate mysql drive connection

--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -11,7 +11,7 @@ import java.sql.ResultSet;
  */
 public class ConnectionFactory {
 
-    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost:3306/todoappjava";
     public static final String USER = "root";
     public static final String PASS = "";
